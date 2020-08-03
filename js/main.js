1d8cbf612ead45fa10913b46d71c69e066201c4a
kriteesh <kriteesh.parashar@gmail.com>
2020-08-04 02:47:25 +0530

    wow

--- js/main.js
+++ js/main.js
@@ -12,6 +12,16 @@ righthead = create(header)('div')('righthead')({});
 
 righthead.innerText = heading.split(" ")[1];
 
+
+// chooserOne = create(container)('select')('chooserOne')({});
+
+// optionsOne = ['Bhasha','Ganita','Chhanda'].map(x=>{
+//     y = create(chooserOne)('option')('options')({});
+//     y.innerText = x;
+//     y.value = x;
+//     return y;
+// });
+
 area = create(container)('textarea')('area')({placeholder:'Type here in Harvard-Kyoto Protocol\n\nType any random numbers as well to see the magic'});
 
 chooser = create(container)('select')('chooser')({});
@@ -23,7 +33,7 @@ options = choosy.map(x=>{
     return y;
 });
 
-reflection = create(container)('textarea')('reflection')({readonly:true});
+reflection = create(container)('textarea')('area')({});
 
 footer = create(container)('div')('footer')({});
 
@@ -39,8 +49,8 @@ nasalSoundCorr = arr => arr.replace(/[JNnm](?=k|g)/g,'G').replace(/[GNnm](?=c|j)
 twist = (lang) =>(val)=> { 
     if(val==null) reflection.innerHTML = '';
     else {
-        if(chooser.value=='english') reflection.innerHTML = hkToIast(area.value);
-        else reflection.innerHTML = devToLang(hkToDev(val))(lang);
+        if(chooser.value=='english') reflection.innerHTML = hkToIast(val);
+        else reflection.innerHTML = devToLang(hkToDev(val))(lang) + '\n' + '{' + devToLang(akshara(hkToDev(val)).filter(x=>x!=' ').join('-'))(lang) + '}' + '{' + guruLaghu(akshara(hkToDev(val)).filter(x=>x!=' ')).join('-') + '}';
     }  
 }
 
@@ -62,7 +72,7 @@ chooser.onchange = () =>{
     if(chooser.value == 'english'){
         reflection.innerText = hkToIast(area.value);
     }
-    else reflection.innerText = devToLang(numero(hkToDev(area.value)))(chooser.value.split(","));
+    else reflection.innerHTML = devToLang(numero(hkToDev(area.value)))(chooser.value.split(",")) + '\n' + '{' + devToLang(akshara(hkToDev(area.value)).filter(x=>x!=' ').join('-'))(chooser.value.split(",")) + '}' + '{' + guruLaghu(akshara(hkToDev(area.value)).filter(x=>x!=' ')).join('-') + '}';
 }
 
 destroyClickedElement = (event) => document.body.removeChild(event.target);
@@ -92,6 +102,5 @@ copier.onclick = () => {
     document.execCommand("copy");
 
 }
-
 
 
